103759e8b25cc20bbf581e7cffacf8734a2c3249
mreverson <mreverson91@gmail.com>
2020-08-20 22:58:03 -0700

    header cleanup and build

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,6 @@ function Header() {
                 <input placeholder="Search Slack Clone" />
             </div>
             <div className="header__right">
-                {/* Help Icon */}
                 <HelpOutlineIcon />
             </div>
         </div>
